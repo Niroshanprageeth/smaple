1692081c0b4d97190cfe1a3cbefe15d569aa5f65
Niroshanprageeth <Niroshanprageeth123@gmail.com>
2023-01-21 11:24:48 +0530

    im undated

--- sum.java
+++ sum.java
@@ -7,11 +7,8 @@ class sum{
         int sum=a+b;
 
         System.out.println("Sum is:"+sum);
-        //this is from local machine 1
+       //this is from local machine 1
         // this is a summation here
         // currently working here
-        //task 1
-        //task2
-        // i am editing this from brach sub
     }
 }
